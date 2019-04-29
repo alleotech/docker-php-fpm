36cff6776191573d9f6945ce6a711ab680a4e6f3
Alexander Mamchenkov <mamtchenkov@gmail.com>
2019-04-29 18:28:14 +0300

    Consider commented out lines in run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ then
 fi
 
 echo "Adjusting PHP memory limit to [$PHP_MEMORY_LIMIT]"
-sed -i -e "s/^memory_limit =.*$/memory_limit = $PHP_MEMORY_LIMIT/g" /etc/php.ini
+sed -i -e "s/^;?memory_limit =.*$/memory_limit = $PHP_MEMORY_LIMIT/g" /etc/php.ini
 
 if [[ -z "$PHP_ERROR_REPORTING" ]];
 then
@@ -14,7 +14,7 @@ then
 fi
 
 echo "Adjusting PHP error reporting to [$PHP_ERROR_REPORTING]"
-sed -i -e "s/^error_reporting =.*$/error_reporting = $PHP_ERROR_REPORTING/g" /etc/php.ini
+sed -i -e "s/^;?error_reporting =.*$/error_reporting = $PHP_ERROR_REPORTING/g" /etc/php.ini
 
 if [[ -z "$PHP_POST_MAX_SIZE" ]];
 then
@@ -22,7 +22,7 @@ then
 fi
 
 echo "Adjusting PHP post max size to [$PHP_POST_MAX_SIZE]"
-sed -i -e "s/^post_max_size = .*$/post_max_size = $PHP_POST_MAX_SIZE/g" /etc/php.ini
+sed -i -e "s/^;?post_max_size = .*$/post_max_size = $PHP_POST_MAX_SIZE/g" /etc/php.ini
 
 if [[ -z "$PHP_UPLOAD_MAX_FILESIZE" ]];
 then
@@ -30,7 +30,7 @@ then
 fi
 
 echo "Adjusting PHP upload max file size to [$PHP_UPLOAD_MAX_FILESIZE]"
-sed -i -e "s/^upload_max_filesize = .*$/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/g" /etc/php.ini
+sed -i -e "s/^;?upload_max_filesize = .*$/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/g" /etc/php.ini
 
 if [[ -z "$PHP_SESSION_SAVE_PATH" ]];
 then
@@ -38,7 +38,7 @@ then
 fi
 
 echo "Adjusting PHP session save path to [$PHP_SESSION_SAVE_PATH]"
-sed -i -e "s~^session\.save_path = .*$~session\.save_path = $PHP_SESSION_SAVE_PATH~g" /etc/php.ini
+sed -i -e "s~^;?session\.save_path = .*$~session\.save_path = $PHP_SESSION_SAVE_PATH~g" /etc/php.ini
 mkdir -p $PHP_SESSION_SAVE_PATH
 chown nobody: /var/lib/php -R
 chown nobody: $PHP_SESSION_SAVE_PATH -R
@@ -49,6 +49,6 @@ then
 fi
 
 echo "Adjusting PHP-FPM max children to [$FPM_MAX_CHILDREN]"
-sed -i -e "s/^pm.max_children = .*$/pm.max_children = $FPM_MAX_CHILDREN/g" /etc/php-fpm.d/www.conf
+sed -i -e "s/^;?pm.max_children = .*$/pm.max_children = $FPM_MAX_CHILDREN/g" /etc/php-fpm.d/www.conf
 
 /usr/sbin/php-fpm -F -R
